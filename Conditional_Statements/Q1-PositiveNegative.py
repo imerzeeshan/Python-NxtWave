948ae258aea8b94d3a6034d2879c3ad9ed68c3be
Md Zeeshan <imerzeeshan@gmail.com>
2024-09-21 21:18:45 +0530

    check positive negative number

---
+++ Conditional_Statements/Q1-PositiveNegative.py
@@ -0,0 +1,25 @@
+# Write a program that reads a number N and checks if the number N is a Negative Number or a Positive Number.
+
+# Print Positive If N is greater than 0.
+# Print Negative if N is less than 0.
+
+# Input
+# The input will be a single line containing a float representing 
+
+# Output
+# The output should be a single line containing a string. Positive
+# should be printed if N is greater than 0. Negative should be
+# printed if N is less than 0.
+
+# Constraints
+# The given number N is not equal to 0.
+
+
+
+
+n = float(input())
+
+if n > 0:
+    print("Positive")
+else:
+    print("Negative")
