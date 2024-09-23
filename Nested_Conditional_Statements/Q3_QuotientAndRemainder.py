959a426920dddfce6a3e3500e2d040662f5f6d83
Md Zeeshan <imerzeeshan@gmail.com>
2024-09-23 11:07:59 +0530

    Quotient and Remainder

---
+++ Nested_Conditional_Statements/Q3_QuotientAndRemainder.py
@@ -0,0 +1,10 @@
+# Write a program that reads two numbers A and B and prints the Quotient and Remainder when A is divided by B.
+
+a = int(input())
+b = int(input())
+
+quotient = int(a / b)
+remainder = a % b
+
+print(quotient)
+print(remainder)
