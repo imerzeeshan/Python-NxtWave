353b8a4cb5afeae59fa10f0068c6e8038c37e819
Md Zeeshan <imerzeeshan@gmail.com>
2024-09-26 23:12:24 +0530

    day name

---
+++ Conditional_Statements/Q12-dayName.py
@@ -0,0 +1,22 @@
+# Question: Day Name
+# In this coding question, you need to write a program that reads a day number and prints the corresponding day name. For example,
+# if the given day number is 1, the day name is Monday; 
+# if the given day number is 2, the day name is Tuesday, and so on.
+
+
+dayNumber = int(input())
+
+if dayNumber == 1:
+    print("Monday")
+elif dayNumber == 2:
+    print("Tuesday")
+elif dayNumber == 3:
+    print("Wednesday")
+elif dayNumber == 4:
+    print("Thursday")
+elif dayNumber == 5:
+    print("Friday")
+elif dayNumber == 6:
+    print("Saturday")
+elif dayNumber == 7:
+    print("Sunday")
