87a6793557c62f53daada3ef1e5af4cb42d943b6
Md Zeeshan <imerzeeshan@gmail.com>
2024-09-27 17:19:35 +0530

    to print Even or odd Number

---
+++ Nested_Conditional_Statements/Q4_EvenOdd.py
@@ -0,0 +1,9 @@
+# Write a program that reads a number N and checks if Nis divisible by 2.
+# Print Even if N is divisible by 2. Otherwise, print Odd.
+
+n = int(input())
+
+if (n % 2) == 0:
+    print("Even")
+else:
+    print("Odd")
