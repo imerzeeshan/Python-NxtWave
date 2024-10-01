1dccbab1d1adf83a7f75e72772b1da8cc5eec604
Md Zeeshan <imerzeeshan@gmail.com>
2024-10-01 19:39:10 +0530

    finding square root in python

---
+++ Nested_Conditional_Statements/Q15_SquareRoot.py
@@ -0,0 +1,7 @@
+# In this coding question, you need to
+#  write a program that reads a number N and prints the square root of N. 
+# To calculate the square root of N, you can use the formula N ** 0.5.
+
+n = int(input())
+
+print(n ** 0.5)
